8772be26d42b5e0ec885dc2febce65b1456ad9cb
0x0D <0xOD@proton.me>
2026-04-17 19:32:58 +0200

    feat: Added Mochii::Scope and Mochii::Ref definitions

--- Mochii/src/Mochii/Core.h
+++ Mochii/src/Mochii/Core.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 
 #ifdef MI_PLATFORM_WINDOWS
 	#if MI_DYNAMIC_LINK
@@ -29,3 +30,11 @@
 #define BIT(x) (1 << x)
 
 #define MI_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Mochii {
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+}
