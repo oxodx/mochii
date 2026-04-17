63a41ae69604de69548630068682d121690edf91
0x0D <0xOD@proton.me>
2026-04-17 23:33:58 +0200

    chore: Migrate to cmake and abandon visual studio

--- premake/fetch.lua
+++
@@ -1,5 +0,0 @@
-function fetch(name, url, tag, path)
-    if not os.isdir(path) then
-        os.execute("git clone --depth=1 --branch " .. tag .. " " .. url .. " " .. path)
-    end
-end
--- scripts/Win-GenProjects.bat
+++ scripts/Win-GenProjects.bat
@@ -1,5 +1,11 @@
 @echo off
-pushd ..\
-call premake5 vs2026
-popd
+cd ..
+mkdir build 2>nul
+cd build
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug
+cd ..
+cmake --build build
+echo D | xcopy /s /y .\Sandbox\assets\ .\build\bin\assets
+cd .\build\bin
+.\Sandbox.exe
 PAUSE
