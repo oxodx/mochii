9f70b739afeb6a0386c00f2b05ee23fd5ecba005
0x0D <0xOD@proton.me>
2026-04-18 09:31:30 +0200

    feat: Enhance OpenGL context initialization and shader compilation error handling

--- Mochii/src/Mochii/Platform/OpenGL/OpenGLContext.cpp
+++ Mochii/src/Mochii/Platform/OpenGL/OpenGLContext.cpp
@@ -19,6 +19,15 @@ namespace Mochii {
 		MI_CORE_INFO("  Vendor: {}", reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
 		MI_CORE_INFO("  Renderer: {}", reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
 		MI_CORE_INFO("  Version: {}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
+
+		#ifdef MI_ENABLE_ASSERTS
+			int versionMajor;
+			int versionMinor;
+			glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+			glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+			MI_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Mochii requires at least OpenGL version 4.5!");
+		#endif
 	}
 
 	void OpenGLContext::SwapBuffers() {
