45f184c2a2b8adca2e66bda81d5d2a6efb01627e
0x0D <0xOD@proton.me>
2026-04-21 13:50:55 +0200

    fix: use std::array for samplers to avoid VLA ub

--- Mochii/src/Mochii/Renderer/Renderer2D.cpp
+++ Mochii/src/Mochii/Renderer/Renderer2D.cpp
@@ -79,13 +79,13 @@ void Renderer2D::Init() {
   uint32_t whiteTextureData = 0xffffffff;
   s_Data.WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
 
-  int32_t samplers[s_Data.MaxTextureSlots];
-  for (uint32_t i = 0; i < s_Data.MaxTextureSlots; i++) samplers[i] = i;
+  std::array<int32_t, Renderer2DData::MaxTextureSlots> samplers;
+  for (uint32_t i = 0; i < Renderer2DData::MaxTextureSlots; i++) samplers[i] = i;
 
   s_Data.TextureShader = Shader::Create("assets/shaders/Texture.glsl");
   s_Data.TextureShader->Bind();
-  s_Data.TextureShader->SetIntArray("u_Textures", samplers,
-                                    s_Data.MaxTextureSlots);
+  s_Data.TextureShader->SetIntArray("u_Textures", samplers.data(),
+                                  Renderer2DData::MaxTextureSlots);
 
   s_Data.TextureSlots[0] = s_Data.WhiteTexture;
 
