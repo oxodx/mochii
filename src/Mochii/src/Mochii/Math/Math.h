b8f04dad321e1092e656b1fe632c1a0d88d1faca
0x0D <0xOD@proton.me>
2026-04-25 19:34:43 +0200

    feat: enhance framebuffer specifications with texture formats and update color attachment retrieval

--- src/Mochii/src/Mochii/Math/Math.h
+++ src/Mochii/src/Mochii/Math/Math.h
@@ -2,6 +2,6 @@
 #include <glm/glm.hpp>
 
 namespace Mochii::Math {
-bool DecomposeTransform(const glm::mat4& transform, glm::vec3& outTranslation,
-                        glm::vec3& outRotation, glm::vec3& outScale);
+bool DecomposeTransform(const glm::mat4& transform, glm::vec3& translation,
+                        glm::vec3& rotation, glm::vec3& scale);
 }
--- src/Mochii/src/Mochii/Platform/OpenGL/OpenGLFramebuffer.h
+++ src/Mochii/src/Mochii/Platform/OpenGL/OpenGLFramebuffer.h
@@ -14,8 +14,10 @@ class OpenGLFramebuffer : public Framebuffer {
 
   virtual void Resize(uint32_t width, uint32_t height) override;
 
-  virtual uint32_t GetColorAttachmentRendererID() const override {
-    return m_ColorAttachment;
+  virtual uint32_t GetColorAttachmentRendererID(
+      uint32_t index = 0) const override {
+    MI_CORE_ASSERT(index < m_ColorAttachments.size());
+    return m_ColorAttachments[index];
   }
 
   virtual const FramebufferSpecification& GetSpecification() const override {
@@ -24,8 +26,13 @@ class OpenGLFramebuffer : public Framebuffer {
 
  private:
   uint32_t m_RendererID = 0;
-  uint32_t m_ColorAttachment = 0;
-  uint32_t m_DepthAttachment = 0;
   FramebufferSpecification m_Specification;
+
+  std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
+  FramebufferTextureSpecification m_DepthAttachmentSpecification =
+      FramebufferTextureFormat::None;
+
+  std::vector<uint32_t> m_ColorAttachments;
+  uint32_t m_DepthAttachment = 0;
 };
 }  // namespace Mochii
